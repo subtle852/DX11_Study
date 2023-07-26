1453e1a06ba8d5ad2b82777197f8c4d8a5190300
subtle852 <subtle852@gmail.com>
2023-07-27 01:32:05 +0900

    Implement 2d Light

--- YamYamEngine/YamYamEngine/yaPlayScene.cpp
+++ YamYamEngine/YamYamEngine/yaPlayScene.cpp
@@ -79,7 +79,17 @@ namespace ya
 			AddGameObject(eLayerType::Light, light);
 			Light* lightComp = light->AddComponent<Light>();
 			lightComp->SetType(eLightType::Directional);
-			lightComp->SetColor(Vector4(1.0f, 0.0f, 1.0f, 1.0f));
+			lightComp->SetColor(Vector4(1.0f, 1.0f, 1.0f, 1.0f));
+		}
+
+		{
+			GameObject* light = new GameObject();
+			light->SetName(L"Smile");
+			AddGameObject(eLayerType::Light, light);
+			Light* lightComp = light->AddComponent<Light>();
+			lightComp->SetType(eLightType::Point);
+			lightComp->SetColor(Vector4(0.0f, 0.0f, 1.0f, 1.0f));
+			lightComp->SetRadius(3.0f);
 		}
 
 		//{
